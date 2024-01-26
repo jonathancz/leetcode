86d861485437291fac58e1bce791ad03dad19345
Ethan Chen <jonathanchenzheng96@gmail.com>
2024-01-25 20:53:34 -0500

    Optimize Two Sum

--- LeetCode/LeetCode/Easy/TwoSum.cs
+++ LeetCode/LeetCode/Easy/TwoSum.cs
@@ -25,13 +25,65 @@ namespace LeetCode.Easy;
 public class TwoSum
 {
     [Theory]
-    [InlineData(new int[] { 2, 7, 11, 15 }, 9, new [] {0 ,1})]
+    [InlineData(new int[] { 2, 7, 11, 15 }, 9, new[] { 0, 1 })]
     [InlineData(new int[] { 3, 2, 4 }, 6, new[] { 1, 2 })]
     [InlineData(new int[] { 3, 3 }, 6, new[] { 0, 1 })]
     public void Solution(int[] nums, int target, int[] expectedResult)
     {
         var result = GetTwoSums(nums, target);
-        // Assert.True(result.SequenceEqual(expectedResult));
+        var result2 = GetTwoSumsDictionary(nums, target);
+        Assert.True(result.SequenceEqual(expectedResult));
+        Assert.True(result2.SequenceEqual(expectedResult));
+    }
+
+    /// <summary>
+    /// This is optimized because there is only one pass through.
+    /// There is this neat trick:
+    /// First, we go through the array, and subtract each number with the target.
+    /// If the difference is a number that is within the dictionary, we include that in the result.
+    /// If not, continue traversing through the array.
+    /// For instance
+    /// Array: { 3, 2, 4 }
+    /// Target : 6
+    /// We traverse through the array, starting at 3.
+    /// We subtract; 6 - 3 = 3. Is the difference present in the dictionary?
+    /// No, add to dictionary (we add 3 as the key, and value as 0 which is the index), and keep traversing.
+    /// Now, we're at 2.
+    /// 6 - 2 = 4. Is the difference present in the dictionary?
+    /// No, add to dictionary and keep traversing.
+    /// At this point the Dictionary looks like this:
+    ///
+    /// Number Value | Index
+    ///      3       |   0
+    ///      2       |   1
+    ///
+    /// And lastly, we're at 4.
+    /// 6 - 4 = 2. Is the difference in the dictionary? Yes.
+    /// That means that the indices that include the numbers that their sum equals the target is:
+    /// [1, 2]
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    private int[] GetTwoSumsDictionary(int[] nums, int target)
+    {
+        // <Value, Index in the Array>
+        Dictionary<int, int> dictionary = new();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int currentNumber = nums[i];
+            int difference = target - currentNumber;
+            if (dictionary.TryGetValue(difference, out int index))
+            {
+                return new[] { index, i };
+            }
+
+            // If the difference is not found in the dictionary, we want to add it to the dictionary.
+            dictionary[currentNumber] = i;
+        }
+
+        return Array.Empty<int>();
     }
 
     private int[] GetTwoSums(int[] nums, int target)
@@ -45,13 +97,11 @@ public class TwoSum
                 if ((nums[i] + nums[j] == target))
                 {
                     var firstIndex = Array.IndexOf(nums, nums[i]);
-                    var secondIndex = Array.IndexOf(nums, nums[j]);
-                    result.AddRange(new[] { firstIndex, secondIndex });
+                    result.AddRange(new[] { firstIndex, j });
                     return result.ToArray();
                 }
             }
         }
-
         return result.ToArray();
     }
 }
